8d3f227c59f3bf8622f26df7ff9d00c506eb22eb
adityadeshmukh23 <deshmukhaditya052@gmail.com>
2025-06-27 04:46:03 +0530

    Local changes: updated db_connection and .env

--- database/db_connection.py
+++ database/db_connection.py
@@ -2,8 +2,10 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import os
 
-# Load key from project’s secrets folder
-cred = credentials.Certificate(os.path.join("secrets", "firebase_key.json"))
+# Use absolute path to the secrets directory in your home folder
+key_path = os.path.expanduser("~/secrets/firebase_key.json")
+
+cred = credentials.Certificate(key_path)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
