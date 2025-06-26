4c3206ff9cb1dacd56a8830ac940034a9b5ad6c4
adityadeshmukh23 <deshmukhaditya052@gmail.com>
2025-06-27 04:19:32 +0530

    Updated db_connection to use secure path

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,7 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
 
-cred = credentials.Certificate("firebase_key.json")
+# Load key from project’s secrets folder
+cred = credentials.Certificate(os.path.join("secrets", "firebase_key.json"))
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
